b4d1c18226f49a583daed87437d025c38477c29f
khaoulagharib98 <khaoulagharib98@gmail.com>
2022-01-12 22:45:15 +0100

    Ajout de la classe Statut

---
+++ src/main/java/com/example/gestiontaches/bean/Statut.java
@@ -0,0 +1,60 @@
+package com.example.gestiontaches.bean;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Statut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long numero;
+    private String description;
+    @OneToMany(targetEntity = Tache.class, mappedBy = "statut")
+    @JsonManagedReference
+    private List<Tache> taches;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Tache> getTaches() {
+        return taches;
+    }
+
+    public void setTaches(List<Tache> taches) {
+        this.taches = taches;
+    }
+
+    @Override
+    public String toString() {
+        return "Statut{" +
+                "id=" + id +
+                ", numero=" + numero +
+                ", description='" + description + '\'' +
+                ", taches=" + taches +
+                '}';
+    }
+}
